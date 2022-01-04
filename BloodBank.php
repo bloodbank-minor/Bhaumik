a06ebc56ec1e3b748a0f05fcb1a5a7b3e7020b72
BhaumikTiwari <97089417+BhaumikTiwari@users.noreply.github.com>
2022-01-04 20:09:15 +0530

    Add files via upload

---
+++ BloodBank.php
@@ -0,0 +1,351 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>blood bank</title>
+  </head>
+  <body>
+  
+    <nav class="navbar navbar-expand-lg py-3 navbar-dark bg-danger shadow-sm">
+  <div class="container">
+    <a href="#" class="navbar-brand">
+      <!-- Logo Image -->
+      <img src="img/SANJEEVNI.png" height="70" width="70" >
+      <!-- Logo Text -->
+      <span class=" font-weight-bold">Sanjeevni-saving lives</span>
+    </a>
+
+
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">looking for blood</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="#">want to donate</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Blood Bank register/login
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">register</a></li>
+            <li><a class="dropdown-item" href="#">log in</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/tp227-facebookeventcover-04.jpg" height="600" width="1600" >
+      <div class="carousel-caption d-none d-md-block">
+        <h5>WELCOME TO SANJEEVNI</h5>
+        <p>Excuses never save a life, Blood donation does..</p>
+        <button class="btn btn-primary">sign up/register</button>
+        <button class="btn btn-success">login</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+        <img src="img/—Pngtree—blood donation public welfare poster_996183.jpg" height="600" width="1600" />
+      <div class="carousel-caption d-none d-md-block">
+        <h4 style="color:black" >DONT WORRY !!</h4>
+        <p style="color:black">Donating blood has no side effects</p>
+        <button class="btn btn-success">looking for blood</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/earth-4861456_1920.jpg" height="600" width="1600">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>BECOME A HERO!!</h5>
+        <p>“Blood is a life, pass it on!”</p>
+        <button class="btn btn-danger">donate now</button>
+      </div>
+    </div>
+      <div class="carousel-item">
+          <img src="img/blood-1968457_1280.png" height="600" width="1600">
+      <div class="carousel-caption d-none d-md-block">
+        <h4 style="color:black">BLOOD GROUPS</h4>
+        <p style="color:black">every group is precious</p>
+        <button class="btn btn-primary">Learn about donation</button>
+      </div>
+    </div>
+      <div class="carousel-item">
+          <img src="img/Project.jpg"  height="600" width="1600">
+      <div class="carousel-caption d-none d-md-block">
+        <h4 style="color:black">GIVE A HELPING HAND</h4>
+        <p style="color:black">That hand will get tons of blessings</p>
+      </div>
+    </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<div class="row" style="padding-top:4px;">
+       <div class="row text-center">
+       <div class="col-md-12 typeHeading">
+       <h1 class="learnAbout">Learn About Donation</h1>
+       </div>
+       </div>
+       <br><br>
+       <div class="wow col-sm-6 rotateInDownRight text-center animated" data-wow-delay="0ms" data-wow-duration="1000ms" style="visibility: visible; animation-duration: 1000ms; animation-delay: 0ms; animation-name: rotateInDownRight;">
+       <!-- <img src='assets/donationFact.jpg'></img> -->
+       
+          <img src="img/donationFact.jpg" width="500" alt="One Donation Can save upto three lives">
+
+       <blockquote>
+ 		 <p style="font-family:oswald">After donating blood, the body works to replenish the blood loss. This stimulates the production of new blood cells and in turn, helps in maintaining good health.</p><p>
+		</p></blockquote>
+       
+      	<button onclick="window.location='/BLDAHIMS/bloodbank/nearbyBBRed.cnt'" class="btn btn-danger svcNearBy hvr-sweep-to-right "><i class="fa fa-tint" aria-hidden="true"></i>&nbsp; Donate Now</button> 
+      <br><br>
+       </div>
+			
+			
+			<div class="col-sm-6">
+			<table class="table table-responsive table-bordered ">
+<tbody>
+<tr>
+<th colspan="3" style="color:white;background-color:red;">Compatible Blood Type Donors</th>
+</tr>
+<tr>
+<td><b>Blood Type</b></td>
+<td><b>Donate Blood To</b></td>
+<td><b>Receive Blood From</b></td>
+</tr>
+<tr>
+<td><span style="color: #961e1b;"><b>A+</b></span></td>
+<td>A+ AB+</td>
+<td>A+ A- O+ O-</td>
+</tr>
+<tr>
+<td><span style="color: #961e1b;"><b>O+</b></span></td>
+<td>O+ A+ B+ AB+</td>
+<td>O+ O-</td>
+</tr>
+<tr>
+<td><span style="color: #961e1b;"><b>B+</b></span></td>
+<td>B+ AB+</td>
+<td>B+ B- O+ O-</td>
+</tr>
+<tr>
+<td><span style="color: #961e1b;"><b>AB+</b></span></td>
+<td>AB+</td>
+<td>Everyone</td>
+</tr>
+<tr>
+<td><span style="color: #961e1b;"><b>A-</b></span></td>
+<td>A+ A- AB+ AB-</td>
+<td>A- O-</td>
+</tr>
+<tr>
+<td><span style="color: #961e1b;"><b>O-</b></span></td>
+<td>Everyone</td>
+<td>O-</td>
+</tr>
+<tr>
+<td><span style="color: #961e1b;"><b>B-</b></span></td>
+<td>B+ B- AB+ AB-</td>
+<td>B- O-</td>
+</tr>
+<tr>
+<td><span style="color: #961e1b;"><b>AB-</b></span></td>
+<td>AB+ AB-</td>
+<td>AB- A- B- O-</td>
+</tr>
+</tbody>
+</table>
+</div>
+			
+</div> 
+          <hr size="15" width="100%" color="red">
+          <div class="col-md-8 col-md-offset-2 donationTypeInfoWindow text-center">
+			<header class="typeHeading">
+				<h1>Types of donation</h1>
+			</header>
+			<!-- /Header -->
+
+			<!-- Content -->
+			<div class="content">
+                <p style="text-align: center;">The human body contains five liters of blood, which is made of several useful components i.e. <strong>Whole blood</strong>, <strong>Platelet</strong>, and <strong>Plasma</strong>.</p>
+                <p style="text-align: center;">Each type of component has several medical uses and can be used for different medical treatments. your blood donation determines the best donation for you to make.</p>
+                <p style="text-align: center;">For <strong>plasma</strong> and <strong>platelet</strong> donation you must have donated whole blood in past two years.</p>
+                							
+            </div>
+            <br><br>
+            
+		
+		<div class="row tabInfo" id="wholeBlood">
+		<div class="col-md-4 ">
+		
+			<h3>What is it?</h3>
+			
+			<p>
+			Blood Collected straight from the donor after its donation usually separated into red blood cells, platelets, and plasma.</p>
+			
+			<h3>Who can donate?</h3>
+			
+			<p>You need to be 18-65 years old, weigh 45kg or more and be fit and healthy.</p>
+		
+
+		</div>
+		<div class="col-md-4 infoContent">
+		<h3>User For?</h3>
+			
+			<p>
+			 Stomach disease, kidney disease, childbirth, operations, blood loss, trauma, cancer, blood diseases, haemophilia, anemia, heart disease.</p>
+			
+			<h3>Lasts For?</h3>
+			
+			<p>Red cells can be stored for 42 days.</p>
+		
+		</div>
+		<div class="col-md-4 ">
+		<h3>How long does it take?</h3>
+			
+			<p>
+			15 minutes to donate.</p>
+			
+			<h3>How often can I donate?</h3>
+			
+			<p>Every 12 weeks</p>
+		
+		</div>
+		</div>
+		
+		<div class="row tabInfo" style="display:none" id="plasma">
+		<div class="col-md-4 ">
+		
+			<h3>What is it?</h3>
+			
+			<p>
+			The straw-coloured liquid in which red blood cells, white blood cells, and platelets float in.Contains special nutrients which can be used to create 18 different type of medical products to treat many different medical conditions.</p>
+			
+			<h3>Who can donate?</h3>
+			
+			<p>You need to be 18-70 (men) or 20-70 (women) years old, weigh 50kg or more and must have given successful whole blood donation in last two years. </p>
+		
+
+		</div>
+		<div class="col-md-4 infoContent">
+		<h3>User For?</h3>
+			
+			<p>
+			Immune system conditions, pregnancy (including anti-D injections), bleeding, shock, burns, muscle and nerve conditions, haemophilia, immunisations.</p>
+			
+			<h3>Lasts For?</h3>
+			
+			<p>Plasma can last up to one year when frozen.</p>
+		
+		</div>
+		<div class="col-md-4 ">
+		<h3>How  does it work?</h3>
+			
+			<p>
+			We collect your blood, keep plasma and return rest to you by apheresis donation.</p>
+		
+		<h3>How long does it take?</h3>
+			
+			<p>
+			15 minutes to donate.</p>
+			
+			<h3>How often can I donate?</h3>
+			
+			<p>Every 2-3 weeks.</p>
+		
+		</div>
+		</div>
+		
+		<div class="row tabInfo" style="display:none" id="platelet">
+		<div class="col-md-4 ">
+		
+			<h3>What is it?</h3>
+			
+			<p>
+			The tiny 'plates' in blood that wedge together to help to clot and reduce bleeding. Always in high demand, Vital for people with low platelet count, like malaria and cancer patients.</p>
+			
+			<h3>Who can donate?</h3>
+			
+			<p>You need to be 18-70 years old (men), weigh 50kg or more and have given a successful plasma donation in the past 12 months</p>
+		
+
+		</div>
+		<div class="col-md-4 infoContent">
+		<h3>User For?</h3>
+			
+			<p>
+			Cancer, blood diseases, haemophilia, anaemia, heart disease, stomach disease, kidney disease, childbirth, operations, blood loss, trauma, burns.</p>
+			
+			<h3>Lasts For?</h3>
+			
+			<p>Just five days..</p>
+		
+		</div>
+		<div class="col-md-4 ">
+		<h3>How does it work?</h3>
+			
+			<p>
+			We collect your blood, keep platelet and return rest to you by apheresis donation.</p>
+			
+		<h3>How long does it take?</h3>
+			
+			<p>
+			45 minutes to donate.</p>
+			
+			<h3>How often can I donate?</h3>
+			
+			<p>Every 2 weeks</p>
+		
+		</div>
+		</div>
+		<br><br>
+		<button type="button" onclick="window.location='/BLDAHIMS/bloodbank/nearbyBBRed.cnt'" style="border-radius:0px" class="btn-lg btn btn-danger svcNearBy hvr-sweep-to-right ">Find Nearest Blood Bank To Donate</button> 
+		<br><br>
+</div>
+          
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+    
+  </body>
+</html>
